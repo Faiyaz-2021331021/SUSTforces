ea14cc3f261beb241ef30e6efe1dfb77ced3d855
Faiyaz <faiyazismail111@gmail.com>
2025-11-19 12:43:05 +0600

    created server

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Backend is running!");
+});
+
+app.listen(5000, () => {
+    console.log("Server started at http://localhost:3000");
+});
+require("./src/config/db"); // connect DB
